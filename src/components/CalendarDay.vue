95d391ad013fbb843e553f4eb934ffead80567ae
Arthur Frank <frank.artur0303@gmail.com>
2019-01-19 00:02:58 +0000

    initial commit

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="c-calendar-day">
+    <p class="light-text">{{weekday}}</p>
+    <p>{{date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarDay',
+  props: {
+    date: Number,
+    weekday: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import './../scss/colors';
+
+.c-calendar-day {
+  width: 100px;
+  height: 100px;
+  margin: 5px;
+  border: 1px solid $color-grey;
+
+  .light-text {
+    color: $color-grey-darker;
+  }
+}
+</style>
